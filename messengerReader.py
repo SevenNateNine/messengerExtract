4405755acf05c810c92e8f58eeefc74509e0a525
SevenNateNine <nate.chan29@gmail.com>
2020-05-23 09:10:31 -0400

    Implemented basic functionality

--- messengerReader.py
+++ messengerReader.py
@@ -1,14 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.options import Options
+
 from time import sleep
 import platform
 
 username = ''
 password = ''
+friend = '' # account for nicknames
+searchkey = ''
 
-def messengerLogin():
-    driver = webdriver.Chrome('chromedriver.exe')
+def messengerLogin(driver):
     driver.get('https://www.messenger.com/')
     sleep(5)
     driver.find_element_by_xpath('//*[@id="email"]').send_keys(username)
@@ -16,6 +19,32 @@ def messengerLogin():
     sleep(2)
     driver.find_element_by_xpath('//*[@id="loginbutton"]').click()
 
-print("logging in to messenger")
-messengerLogin()
-print("action complete")
+def messengerConvo(driver):
+    driver.find_element_by_xpath('//*[contains(text(), "' + friend + '")]').click()
+    sleep(1)
+    driver.get(driver.current_url + "?q=" + searchkey)
+    sleep(2)
+    driver.find_element_by_xpath('//*[@id="facebook"]/body/div[2]/div[2]/div/div/div/div/div/div/div/div[2]/div/span[1]/button').click()
+    sleep(1)
+    t = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[2]/span/div[2]/div[2]/div[2]/div[1]').text
+    resultNum = int(t.split()[2])
+    for i in range(0, resultNum):
+        driver.save_screenshot("search"+str(i)+".jpg")
+        driver.find_element_by_xpath('/html/body/div[1]/div/div/div[2]/span/div[2]/div[2]/div[2]/span[1]/button').click()
+        sleep(1)
+    
+
+def messengerMain():
+    chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True)
+
+    driver = webdriver.Chrome('chromedriver.exe', chrome_options = chrome_options)
+
+    print("logging in to messenger")
+    messengerLogin(driver)
+    print("action complete")
+    print("going to conversation message")
+    messengerConvo(driver)
+    print("action complete")
+
+messengerMain()
